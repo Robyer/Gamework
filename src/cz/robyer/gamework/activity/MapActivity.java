10c23696b55ccda87e861addf319d7fcd7f9c511
Robyer <robyer@seznam.cz>
2013-03-16 23:42:23 +0100

    Removed MapActivity menu.

--- src/cz/robyer/gamework/activity/MapActivity.java
+++ src/cz/robyer/gamework/activity/MapActivity.java
@@ -97,13 +97,6 @@ public class MapActivity extends FragmentActivity {
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.map, menu);
-		return true;
-	}
-
-	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		case android.R.id.home:
