69803937dc7c87ba2ebb0633e1dadc0bfbd5e550
Robyer <robyer@seznam.cz>
2013-04-15 11:12:06 +0200

    Tiny cleanup.

--- ExampleGame/src/cz/robyer/gw_example/util/IntentFactory.java
+++ ExampleGame/src/cz/robyer/gw_example/util/IntentFactory.java
@@ -17,8 +17,6 @@ public class IntentFactory {
 	public static Intent createGameServiceIntent(Context context, String filename) {
 		Intent intent = new Intent(context, GameService.class);
 		intent.putExtra("filename", filename);
-		intent.putExtra("package", "cz.robyer.gw_example.activity");
-		intent.putExtra("class", ".GameMapActivity");
 		return intent;
 	}
 }
