4dd7113a6e50f172542006a1c44bab6b0a4d5078
Robyer <robyer@seznam.cz>
2013-05-05 13:08:31 +0200

    Fixed soundArea.

--- Gamework/src/cz/robyer/gamework/scenario/area/SoundArea.java
+++ Gamework/src/cz/robyer/gamework/scenario/area/SoundArea.java
@@ -9,16 +9,18 @@ import cz.robyer.gamework.scenario.Scenario;
 import cz.robyer.gamework.util.GPoint;
 
 /**
- * This represents point area in which space is played some sound.
+ * This represents point area with ability to define sound radius, in which area is played defined sound.
  * @author Robert Pösel
  */
 public class SoundArea extends PointArea {
 	protected String value;
 	protected int soundId = -1;
-	protected float volume = 1.0f;
+	protected float maxVolume = 1.0f;
+	protected float minVolume = 0.0f;
 	protected float pitch = 1.0f;
 	protected int loop = -1; // loop forever
 	protected int soundRadius;
+	protected boolean inSoundRadius = false;
 	
 	/**
 	 * Class constructor
@@ -61,12 +63,27 @@ public class SoundArea extends PointArea {
 	}
 
 	/**
+	 * Sets volume limit for this sound, must be min < max.
+	 * @param min - minimal value, must be >= 0.0f, otherwise will be used 0.0f
+	 * @param max - maximal value, must be <= 1.0f, otherwise will be used 1.0f
+	 */
+	public void setVolumeLimit(float min, float max) {
+		if (min >= max) {
+			Log.e(TAG, String.format("Volume limit is incorrect - min '%.2f' must be lower than max '%.2f'", min, max));
+			return;
+		}
+		
+		this.minVolume = Math.min(Math.max(min, 0.0f), 1.0f);
+		this.maxVolume = Math.max(Math.min(max, 1.0f), 0.0f);
+	}
+	
+	/**
 	 * Calculate sound volume depending on distance
 	 * @param distance in meters
-	 * @return volume from 0.0 to 1.0
+	 * @return volume between minVolume to maxVolume
 	 */
-	private float calcVolume(double distance) {
-		return 1.0f - (float)((soundRadius - distance) / soundRadius);  
+	protected float calcVolume(double distance) {
+		return minVolume + (maxVolume - minVolume) * (float)((soundRadius - distance) / soundRadius); 
 	}
 	
 	/* (non-Javadoc)
@@ -74,29 +91,32 @@ public class SoundArea extends PointArea {
 	 */
 	@Override
 	public void updateLocation(double lat, double lon) {
-		// TODO: fix when soundRadius > radius
+		// update state of inArea and eventually call hooks
+		super.updateLocation(lat, lon);
+		
+		if (soundId == -1) {
+			Log.e(TAG, String.format("Sound '%s' is not loaded", value));
+			return;
+		}
+		
+		// start/stop playing sound or update volume
 		double distance = GPoint.distanceBetween(point.latitude, point.longitude, lat, lon);
-		boolean r = distance < (radius + (inArea ? LEAVE_RADIUS : 0));
+		boolean r = distance < (soundRadius + (inSoundRadius ? LEAVE_RADIUS : 0));
 		float actualVolume = calcVolume(distance);
 
 		SoundPool soundPool = getScenario().getSoundPool();
-		
-		if (inArea != r) {
-			// entering or leaving area
-			Log.i(TAG, String.format("We %s location", (r ? "entered" : "leaved")));
-			inArea = r;
-			callHooks(inArea);
+
+		if (inSoundRadius != r) {
+			inSoundRadius = r;
 			
-			if (soundId != -1) {
-				if (inArea) {
-					Log.d(TAG, String.format("Started playing sound '%s' at volume '%.2f'", value, actualVolume));
-					soundPool.play(soundId, actualVolume, actualVolume, 1, loop, pitch);
-				} else {
-					Log.d(TAG, String.format("Stopped playing sound '%s'", value));
-					soundPool.stop(soundId);
-				}
+			if (inSoundRadius) {
+				Log.d(TAG, String.format("Started playing sound '%s' at volume '%.2f'", value, actualVolume));
+				soundPool.play(soundId, actualVolume, actualVolume, 1, loop, pitch);
+			} else {
+				Log.d(TAG, String.format("Stopped playing sound '%s'", value));
+				soundPool.stop(soundId);
 			}
-		} else if (inArea) {
+		} else if (inSoundRadius) {
 			// update sound volume 
 			Log.d(TAG, String.format("Changed volume of sound '%s' to '%.2f'", value, actualVolume));
 			soundPool.setVolume(soundId, actualVolume, actualVolume);
